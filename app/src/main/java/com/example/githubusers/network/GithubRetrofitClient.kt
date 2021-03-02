f774a2e8467a65e410c66287724ad2ec7f24cf42
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2021-03-02 11:18:48 +0545

    GithubRetrofitClient -> Added logging interceptor

--- app/src/main/java/com/example/githubusers/network/GithubRetrofitClient.kt
+++ app/src/main/java/com/example/githubusers/network/GithubRetrofitClient.kt
@@ -1,6 +1,8 @@
 package com.example.githubusers.network
 
 import com.example.githubusers.Constants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,8 +12,25 @@ object GithubRetrofitClient {
         val retrofit = Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(getOkHttpClient())
             .build()
         return retrofit.create(GithubApi::class.java)
+    }
+
+    private fun getOkHttpClient(): OkHttpClient {
+
+        return OkHttpClient.Builder()
+            .addInterceptor(getLoggingInterceptor())
+            .build()
+
+    }
+
+    private fun getLoggingInterceptor(): HttpLoggingInterceptor {
+
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return loggingInterceptor
+
     }
 
 }
