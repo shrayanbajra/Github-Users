31a3787ac45cad589745580dee7b239be74bd581
shrayan <shrayanbajracharya@gmail.com>
2021-03-16 17:32:41 +0545

    Successfully passed username from UserListFragment to ProfileFragment

--- app/src/main/java/com/example/githubusers/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/githubusers/ui/profile/ProfileFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.githubusers.R
+import com.example.githubusers.util.extensions.getNotAvailableIfNullOrBlank
+import com.example.githubusers.util.extensions.shortToast
 
 class ProfileFragment : Fragment() {
 
@@ -14,6 +16,15 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val args = arguments?.let { ProfileFragmentArgs.fromBundle(it) }
+        val username = args?.username.getNotAvailableIfNullOrBlank()
+        shortToast(message = username)
+
     }
 
 }
--- app/src/main/java/com/example/githubusers/ui/userlist/UserListFragment.kt
+++ app/src/main/java/com/example/githubusers/ui/userlist/UserListFragment.kt
@@ -5,13 +5,13 @@ import android.view.*
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.githubusers.R
 import com.example.githubusers.data.UserCacheEntity
 import com.example.githubusers.databinding.FragmentUserListBinding
 import com.example.githubusers.util.ResultWrapper
-import com.example.githubusers.util.extensions.getNotAvailableIfNullOrBlank
 import com.example.githubusers.util.extensions.shortToast
 
 class UserListFragment : Fragment() {
@@ -29,17 +29,30 @@ class UserListFragment : Fragment() {
         return mBinding.root
     }
 
-    private val mUserAdapter by lazy {
-        UserAdapter(object : UserAdapter.UserClickListener {
+    private val mUserAdapter by lazy { getUserAdapter() }
 
-            override fun onClicked(user: UserCacheEntity) {
-                // TODO: Need to open a profile fragment with details of user
-                val username = user.username.getNotAvailableIfNullOrBlank()
-                shortToast(message = username)
+    private fun getUserAdapter() = UserAdapter(object : UserAdapter.UserClickListener {
+
+        override fun onClicked(user: UserCacheEntity) {
+
+            val username = user.username
+
+            if (username.isNullOrBlank()) {
+                shortToast(message = "Something went wrong")
+                return
             }
 
-        })
-    }
+            navigateToProfileFragment(username)
+
+        }
+
+        private fun navigateToProfileFragment(username: String) {
+            val action = UserListFragmentDirections
+                .actionUserListFragmentToProfileFragment(username = username)
+            findNavController().navigate(action)
+        }
+
+    })
 
     private val mViewModel by lazy {
         ViewModelProvider(this)[UserListViewModel::class.java]
